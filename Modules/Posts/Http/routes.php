03f5c9d617794a5befabe72275dfe327273db94d
Mohammed Manssour <manssour.mohammed@gmail.com>
2018-08-21 09:06:22 +0300

    fix routes namespace

--- Modules/Posts/Http/routes.php
+++ Modules/Posts/Http/routes.php
@@ -2,5 +2,5 @@
 
 Route::prefix('api')
     ->middleware('api')
-    ->namespace('Modules/Posts/Http/Controllers')
+    ->namespace('Modules\Posts\Http\Controllers')
     ->group(module_path('Posts').'/Routes/api.php');

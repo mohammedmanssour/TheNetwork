8e7a7666c4a950756b3ca406e3543b16d902068b
Mohammed Manssour <manssour.mohammed@gmail.com>
2018-08-21 06:04:57 +0300

    Start Files Module

---
+++ Modules/Files/Http/Controllers/FilesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Files\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+
+class FilesController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     * @param  Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * Show the specified resource.
+     * @return Response
+     */
+    public function show()
+    {
+    }
+}

1f72b03a6bfaa90fffcb380dc60556d8df3df8e1
Mohammed Manssour <manssour.mohammed@gmail.com>
2018-08-24 11:35:24 +0300

    add auth middleware to likes and comments

--- Modules/Posts/Routes/api.php
+++ Modules/Posts/Routes/api.php
@@ -5,9 +5,9 @@ if(!app()->environment('testing')){
     $middleware[] = 'auth:api';
 }
 
-Route::delete('posts/{post}/likes','PostsLikesController@destroy');
+Route::delete('posts/{post}/likes','PostsLikesController@destroy')->middleware($middleware);
 Route::apiResource('posts/{post}/likes', 'PostsLikesController',[
     'only' => ['index', 'store']
-]);
+])->middleware($middleware);
 Route::apiResource('posts/{post}/comments', 'CommentsController');
 Route::apiResource('posts','PostsController')->middleware($middleware);
